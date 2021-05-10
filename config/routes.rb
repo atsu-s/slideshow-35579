bf478ef8a1228ad55dd65e7259f46c3660d790c0
曽根惇史 <atsu@soneatsushinoMacBook-Air.local>
2021-05-10 14:58:18 +0900

    トップページ作成・deviseビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'slides/index'
-  root "slides#index"
+  get 'tops/index'
+  root "tops#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
